ffa67c14e5bda0a97e34b07ce16450042dcf7627
rinne-grid <rinne-grid@users.noreply.github.com>
2021-03-27 15:02:15 +0900

    Upd Django REST Framework Request and Response

--- backend/my_api/urls.py
+++ backend/my_api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from my_api import views
 
 urlpatterns = [
     path("snippets/", views.snippet_list),
     path("snippets/<int:pk>/", views.snippet_detail),
 ]
+
+# 特定の形式を参照するためのシンプルできれいな方法を提供
+# http://127.0.0.1/snippets.json
+# http://127.0.0.1/snippets.api
+urlpatterns = format_suffix_patterns(urlpatterns)
